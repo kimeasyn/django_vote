45cd2f7f331fa0ebd4a6a2cb18fa7cd82b8c204b
kimeasyn <kimeasyn@gmail.com>
2017-04-19 00:11:47 +0900

    delete hard coding url

--- myvote/vote/views.py
+++ myvote/vote/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models     import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -17,5 +17,5 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s" % question_id)
 
 def detail(request, question_id):
-    response = "You're lokking at Question : %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'vote/detail.html', {'question': question})
